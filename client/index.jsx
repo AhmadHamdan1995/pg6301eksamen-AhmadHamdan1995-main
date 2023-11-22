31d1367f9b580cdd7d9168c01d4266e67aa6bc33
ahmad hamdan <hmdanahmad594@gmail.com>
2023-11-22 23:21:01 +0100

    Initial commit

---
+++ client/index.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// ... continued from above - replace the `root.render(...)` line
+root.render(<Application />);
+
+function Application() {
+  const [counter, setCounter] = useState(0);
+  return <>
+    <h2>Velkommen til min chat</h2>
+    
+    <div>
+      <button onClick={() => setCounter(oldValue => oldValue + 1)}>Klikk meg</button>
+    </div>
+    <div> Du har klkket {counter} times</div>
+  </>;
+}
+
+
+
